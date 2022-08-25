c090897f2d48759046e63dd6d3d91bbef5e7c66e
Anita <zorrodeplata.anita9@gmail.com>
2022-08-25 14:25:18 +0500

    Lesson 10

---
+++ Lesson_10/Program.cs
@@ -0,0 +1,4 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзначное число: ");
+string str = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine(str[1]);
